401ba3ceafadc27056c1387d11908a668d135aa4
Greshch <greshchook@gmail.com>
2022-07-31 22:56:47 +0300

    Destructor was done

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,7 +1,7 @@
 #include "Vector.h"
 
 Vector::Vector() :
-                    Vector( Vector::capasityDefault)
+                    Vector(Vector::capasityDefault)
 {
 
 }
@@ -11,4 +11,17 @@ Vector::Vector(size_t capasity) :
                     m_buffer(new int[m_capasity])
 {
 
+}
+
+Vector::~Vector()
+{
+    Clear();
+}
+
+void Vector::Clear()
+{
+    delete[] m_buffer;
+    m_buffer = nullptr;
+    m_capasity = capasityDefault;
+    m_size = 0;
 }
