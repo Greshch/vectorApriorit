814ea478556f785bb3ff6fb30a8a5363867a1c8a
Greshch <greshchook@gmail.com>
2022-07-31 22:51:43 +0300

    Constructors were done

--- include/Vector.h
+++ include/Vector.h
@@ -1,13 +1,22 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
+#include <iostream>
+
 class Vector {
 public:
+    Vector();
+    Vector(size_t capasity);
     void PushBack(int num);
     int PopBack();
+    void Clear();
+    ~Vector();
 
 private:
-
+    int* m_buffer = nullptr;
+    size_t m_size = 0;
+    size_t m_capasity;
+    static const size_t capasityDefault = 10;
 };
 
 
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,1 +1,14 @@
 #include "Vector.h"
+
+Vector::Vector() :
+                    Vector( Vector::capasityDefault)
+{
+
+}
+
+Vector::Vector(size_t capasity) :
+                    m_capasity(capasity),
+                    m_buffer(new int[m_capasity])
+{
+
+}
